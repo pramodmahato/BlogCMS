2d359c29d49fe548a067e25480c483dda31e512e
Pramod Mahato <pramodkrmahato@hotmail.com>
2018-10-13 09:35:24 -0700

    Updated Config

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-   define('HOST','localhost');
- define('USERNAME', 'root');
- define('PASSWORD','Inspiron@123');
- define('DB','gdg');
+   define('HOST','SERVER_NAME');
+ define('USERNAME', 'YOUR_USERNAME_HERE');
+ define('PASSWORD','YOUR_PASSWORD_HERE');
+ define('DB','DATABASE_NAME');
    $db = mysqli_connect(HOST,USERNAME,PASSWORD,DB);
 ?>
